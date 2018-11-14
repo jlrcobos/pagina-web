6e3a99fbaeccb1f1ce5cee4ada0d6502be4df8eb
jose luis rodriguez cobos <jlrcobos@gmail.com>
2018-11-14 11:10:58 -0500

    creacion clase persona

---
+++ DEMOWEB/DEMOWEB/Models/Persona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DEMOWEB.Models
+{
+    public class Persona
+    {
+        public string nombres { get;set;}
+    }
+}
